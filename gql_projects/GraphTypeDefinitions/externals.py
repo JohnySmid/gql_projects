cefdfe61a85aeab9b3a98c4569d99911c1916cfc
JohnySmid <junior.smid@gmail.com>
2024-01-15 11:11:43 +0100

    Dataloaders, externall RBACObject

--- gql_projects/GraphTypeDefinitions/externals.py
+++ gql_projects/GraphTypeDefinitions/externals.py
@@ -1,6 +1,8 @@
 import strawberry
 import uuid
 
+from gql_projects.utils.Dataloaders import getLoadersFromInfo
+
 @classmethod
 async def resolve_reference(cls, info: strawberry.types.Info, id: uuid.UUID):
     return cls(id=id)
@@ -25,3 +27,14 @@ class RoleTypeGQLModel:
 class GroupGQLModel:
     id: uuid.UUID = strawberry.federation.field(external=True)
     resolve_reference = resolve_reference
+
+@strawberry.federation.type(extend=True, keys=["id"])
+class RBACObjectGQLModel:
+    id: uuid.UUID = strawberry.federation.field(external=True)
+    resolve_reference = resolve_reference
+
+    @classmethod
+    async def resolve_roles(cls, info: strawberry.types.Info, id: uuid.UUID):
+        loader = getLoadersFromInfo(info).authorizations
+        authorizedroles = await loader.load(id)
+        return authorizedroles
--- gql_projects/utils/Dataloaders.py
+++ gql_projects/utils/Dataloaders.py
@@ -1,14 +1,19 @@
 # efektivní načítání dat z databáze, třída Loaders obsahuje vlastnosti pro načítací funkce pro jednotlivé modely, 
 # a tyto načítací funkce jsou optimalizovány pro efektivní práci s databází a snižují počet dotazů na databázi.
 import datetime
+import aiohttp
+import asyncio
 import logging
 from sqlalchemy import select
 from functools import cache
 import os
 
+from aiodataloader import DataLoader
+
 from gql_projects.DBDefinitions import ProjectCategoryModel, ProjectTypeModel, ProjectModel, MilestoneModel, MilestoneLinkModel, FinanceCategory, FinanceTypeModel, FinanceModel
 
 from uoishelpers.dataloaders import createIdLoader
+from uoishelpers.dataloaders import createIdLoader as createLoader
 
 dbmodels = {
      "projectcategories": ProjectCategoryModel,
@@ -21,121 +26,6 @@ dbmodels = {
      "finances": FinanceModel
 }
 
-#gql_forms dataloaders
-# def prepareSelect(model, where: dict):   
-#     usedTables = [model.__tablename__]
-#     from sqlalchemy import select, and_, or_
-#     baseStatement = select(model)
-#     # stmt = select(GroupTypeModel).join(GroupTypeModel.groups.property.target).filter(GroupTypeModel.groups.property.target.c.name == "22-5KB")
-#     # type(GroupTypeModel.groups.property) sqlalchemy.orm.relationships.RelationshipProperty
-#     # GroupTypeModel.groups.property.entity.class_
-#     def limitDict(input):
-#         if isinstance(input, list):
-#             return [limitDict(item) for item in input]
-#         if not isinstance(input, dict):
-#             # print("limitDict", input)
-#             return input
-#         result = {key: limitDict(value) if isinstance(value, dict) else value for key, value in input.items() if value is not None}
-#         return result
-    
-#     def convertAnd(model, name, listExpr):
-#         assert len(listExpr) > 0, "atleast one attribute in And expected"
-#         results = [convertAny(model, w) for w in listExpr]
-#         return and_(*results)
-
-#     def convertOr(model, name, listExpr):
-#         # print("enter convertOr", listExpr)
-#         assert len(listExpr) > 0, "atleast one attribute in Or expected"
-#         results = [convertAny(model, w) for w in listExpr]
-#         return or_(*results)
-
-#     def convertAttributeOp(model, name, op, value):
-#         # print("convertAttributeOp", type(model))
-#         # print("convertAttributeOp", model, name, op, value)
-#         column = getattr(model, name)
-#         assert column is not None, f"cannot map {name} to model {model.__tablename__}"
-#         opMethod = getattr(column, op)
-#         assert opMethod is not None, f"cannot map {op} to attribute {name} of model {model.__tablename__}"
-#         return opMethod(value)
-
-#     def convertRelationship(model, attributeName, where, opName, opValue):
-#         # print("convertRelationship", model, attributeName, where, opName, opValue)
-#         # GroupTypeModel.groups.property.entity.class_
-#         targetDBModel = getattr(model, attributeName).property.entity.class_
-#         # print("target", type(targetDBModel), targetDBModel)
-
-#         nonlocal baseStatement
-#         if targetDBModel.__tablename__ not in usedTables:
-#             baseStatement = baseStatement.join(targetDBModel)
-#             usedTables.append(targetDBModel.__tablename__)
-#         #return convertAttribute(targetDBModel, attributeName, opValue)
-#         return convertAny(targetDBModel, opValue)
-        
-#         # stmt = select(GroupTypeModel).join(GroupTypeModel.groups.property.target).filter(GroupTypeModel.groups.property.target.c.name == "22-5KB")
-#         # type(GroupTypeModel.groups.property) sqlalchemy.orm.relationships.RelationshipProperty
-
-#     def convertAttribute(model, attributeName, where):
-#         woNone = limitDict(where)
-#         #print("convertAttribute", model, attributeName, woNone)
-#         keys = list(woNone.keys())
-#         assert len(keys) == 1, "convertAttribute: only one attribute in where expected"
-#         opName = keys[0]
-#         opValue = woNone[opName]
-
-#         ops = {
-#             "_eq": "__eq__",
-#             "_lt": "__lt__",
-#             "_le": "__le__",
-#             "_gt": "__gt__",
-#             "_ge": "__ge__",
-#             "_in": "in_",
-#             "_like": "like",
-#             "_ilike": "ilike",
-#             "_startswith": "startswith",
-#             "_endswith": "endswith",
-#         }
-
-#         opName = ops.get(opName, None)
-#         result = convertAttributeOp(model, attributeName, opName, opValue)
-#         return result
-        
-#     def convertAny(model, where):
-        
-#         woNone = limitDict(where)
-#         # print("convertAny", woNone, flush=True)
-#         keys = list(woNone.keys())
-#         # print(keys, flush=True)
-#         # print(woNone, flush=True)
-#         assert len(keys) == 1, "convertAny: only one attribute in where expected"
-#         key = keys[0]
-#         value = woNone[key]
-        
-#         convertors = {
-#             "_and": convertAnd,
-#             "_or": convertOr
-#         }
-#         #print("calling", key, "convertor", value, flush=True)
-#         #print("value is", value, flush=True)
-#         convertor = convertors.get(key, convertAttribute)
-#         convertor = convertors.get(key, None)
-#         modelAttribute = getattr(model, key, None)
-#         if (convertor is None) and (modelAttribute is None):
-#             assert False, f"cannot recognize {model}.{key} on {woNone}"
-#         if (modelAttribute is not None):
-#             property = getattr(modelAttribute, "property", None)
-#             target = getattr(property, "target", None)
-#             # print("modelAttribute", modelAttribute, target)
-#             if target is None:
-#                 result = convertAttribute(model, key, value)
-#             else:
-#                 result = convertRelationship(model, key, where, key, value)
-#         else:
-#             result = convertor(model, key, value)
-#         return result
-    
-#     filterStatement = convertAny(model, limitDict(where))
-#     result = baseStatement.filter(filterStatement)
-#     return result
 
 @cache
 def composeAuthUrl():
@@ -143,6 +33,74 @@ def composeAuthUrl():
     assert "://" in hostname, "probably bad formated url, has it 'protocol' part?"
     assert "." not in hostname, "security check failed, change source code"
     return hostname
+
+
+class AuthorizationLoader(DataLoader):
+
+    query = """query($id: UUID!){result: rbacById(id: $id) {roles {user { id } group { id } roletype { id }}}}"""
+            # variables = {"id": rbacobject}
+
+    roleUrlEndpoint=None#composeAuthUrl()
+    def __init__(self,
+        roleUrlEndpoint=roleUrlEndpoint,
+        query=query,
+        demo=True):
+        super().__init__(cache=True)
+        self.roleUrlEndpoint = roleUrlEndpoint if roleUrlEndpoint else composeAuthUrl()
+        self.query = query
+        self.demo = demo
+        self.authorizationToken = ""
+
+    def setTokenByInfo(self, info):
+        self.authorizationToken = ""
+
+    async def _load(self, id):
+        variables = {"id": f"{id}"}
+        if self.authorizationToken != "":
+            headers = {"authorization": f"Bearer {self.authorizationToken}"}
+        else:
+            headers = {}
+        json = {
+            "query": self.query,
+            "variables": variables
+        }
+        roleUrlEndpoint=self.roleUrlEndpoint
+        async with aiohttp.ClientSession() as session:
+            print(f"query {roleUrlEndpoint} for json={json}")
+            async with session.post(url=roleUrlEndpoint, json=json, headers=headers) as resp:
+                print(resp.status)
+                if resp.status != 200:
+                    text = await resp.text()
+                    print(text)
+                    return []
+                else:
+                    respJson = await resp.json()
+
+        # print(20*"respJson")
+        # print(respJson)
+        
+        assert respJson.get("errors", None) is None, respJson["errors"]
+        respdata = respJson.get("data", None)
+        assert respdata is not None, "missing data response"
+        result = respdata.get("result", None)
+        assert result is not None, "missing result"
+        roles = result.get("roles", None)
+        assert roles is not None, "missing roles"
+        
+        # print(30*"=")
+        # print(roles)
+        # print(30*"=")
+        return [*roles]
+
+
+    async def batch_load_fn(self, keys):
+        #print('batch_load_fn', keys, flush=True)
+        reducedkeys = set(keys)
+        awaitables = (self._load(key) for key in reducedkeys)
+        results = await asyncio.gather(*awaitables)
+        indexedResult = {key:result for key, result in zip(reducedkeys, results)}
+        results = [indexedResult[key] for key in keys]
+        return results
 
 def update(destination, source=None, extraValues={}):
     """Updates destination's attributes with source's attributes.
@@ -160,75 +118,6 @@ def update(destination, source=None, extraValues={}):
 
     return destination
 
-
-# def createLoader(asyncSessionMaker, DBModel):
-#     baseStatement = select(DBModel)
-#     mainstmt = select(DBModel)
-#     print(str(baseStatement)+"test")
-#     class Loader:
-#         async def load(self, id):
-#             async with asyncSessionMaker() as session:
-#                 statement = baseStatement.filter_by(id=id)
-#                 rows = await session.execute(statement)
-#                 rows = rows.scalars()
-#                 row = next(rows, None)
-#                 return row
-        
-#         async def filter_by(self, **kwargs):
-#             async with asyncSessionMaker() as session:
-#                 statement = baseStatement.filter_by(**kwargs)
-#                 rows = await session.execute(statement)
-#                 rows = rows.scalars()
-#                 return rows
-
-#         async def insert(self, entity, extra={}):
-#             newdbrow = DBModel()
-#             newdbrow = update(newdbrow, entity, extra)
-#             async with asyncSessionMaker() as session:
-#                 session.add(newdbrow)
-#                 await session.commit()
-#             return newdbrow
-            
-#         async def update(self, entity, extraValues={}):
-#             async with asyncSessionMaker() as session:
-#                 statement = baseStatement.filter_by(id=entity.id)
-#                 rows = await session.execute(statement)
-#                 rows = rows.scalars()
-#                 rowToUpdate = next(rows, None)
-
-#                 if rowToUpdate is None:
-#                     return None
-
-#                 dochecks = hasattr(rowToUpdate, 'lastchange')             
-#                 checkpassed = True  
-#                 # if (dochecks):
-#                 #     if (entity.lastchange != rowToUpdate.lastchange):
-#                 #         result = None
-#                 #         checkpassed = False                        
-#                 #     else:
-#                 #         entity.lastchange = datetime.datetime.now()
-#                 if checkpassed:
-#                     rowToUpdate = update(rowToUpdate, entity, extraValues=extraValues)
-#                     await session.commit()
-#                     result = rowToUpdate               
-#             return result
-        
-#         async def page(self, skip=0, limit=10, where=None, extendedfilter=None):
-#                     statement = mainstmt
-#                     if where is not None:
-#                         statement = prepareSelect(DBModel, where)
-#                     statement = statement.offset(skip).limit(limit)
-#                     if extendedfilter is not None:
-#                         statement = statement.filter_by(**extendedfilter)
-#                     logging.info(f"loader.page statement {statement}")
-#                     return await self.execute_select(statement)
-#         def set_cache(self, cache_object):
-#             self.cache = True
-#             self._cache = cache_object
-
-#     return Loader(cache=True)
-
-from uoishelpers.dataloaders import createIdLoader as createLoader
 
 class Loaders:
     authorizations = None
@@ -286,6 +175,11 @@ def createLoaders(asyncSessionMaker):
         @cache
         def milestonelinks(self):
             return createLoader(asyncSessionMaker, MilestoneLinkModel)
+        
+        @property
+        @cache
+        def authorizations(self):
+            return AuthorizationLoader()
 
     return Loaders()
 
