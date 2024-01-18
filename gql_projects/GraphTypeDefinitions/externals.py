3eda6cb0d3e958e9e07913d2d68a94b0e7acefbe
JohnySmid <junior.smid@gmail.com>
2024-01-18 18:45:33 +0100

    commented resolve_roles

--- gql_projects/GraphTypeDefinitions/externals.py
+++ gql_projects/GraphTypeDefinitions/externals.py
@@ -43,8 +43,8 @@ class RBACObjectGQLModel:
     id: uuid.UUID = strawberry.federation.field(external=True)
     resolve_reference = resolve_reference
 
-    @classmethod
-    async def resolve_roles(cls, info: strawberry.types.Info, id: uuid.UUID):
-        loader = getLoadersFromInfo(info).authorizations
-        authorizedroles = await loader.load(id)
-        return authorizedroles
+    # @classmethod
+    # async def resolve_roles(cls, info: strawberry.types.Info, id: uuid.UUID):
+    #     loader = getLoadersFromInfo(info).authorizations
+    #     authorizedroles = await loader.load(id)
+    #     return authorizedroles
